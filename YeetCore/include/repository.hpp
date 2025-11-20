2c29ac7e7051e2e0aa1aa8321f741e224834d7e9
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2025-11-21 00:27:13 +0530

    windows

--- YeetCore/include/repository.hpp
+++ YeetCore/include/repository.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#ifdef _WIN32
+    #ifdef _WINNT_
+    #undef _WINNT_
+    #endif
+    #include<windows.h>
+    #include<intrin.h>
+#endif
 #include"../../YeetUtils/include/libs.hpp"
 #include"../../YeetUtils/include/file_utils.hpp"
 #include"../../YeetUtils/include/diff_utils.hpp"
